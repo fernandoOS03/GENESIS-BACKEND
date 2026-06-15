86aa3c1625772965514a35daf98d1aece21ec31a
fernandoOS03 <fernando.192023@outlook.es>
2026-06-15 11:38:48 -0500

    feat : agregar capa para centralizar errores

---
+++ src/main/java/com/goodNews/genesis/core/exceptions/BadRequestException.java
@@ -0,0 +1,10 @@
+package com.goodNews.genesis.core.exceptions;
+
+public class BadRequestException extends RuntimeException {
+	public BadRequestException(String mensaje) {
+		super(mensaje);
+	}
+
+}
+
+
